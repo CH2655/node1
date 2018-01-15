1ece39de8a769bc5318348bd3d337201e5915235
CH2655 <1216011611@qq.com>
2018-01-15 13:10:55 +0800

    添加

---
+++ class15/path.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2018/1/12.
+ */
+const path=require("path");
+
+var str="c:\\wamp\\www\\a.html";
+
+var obj=path.parse(str);
+console.log(obj);
+
+/*{ root: 'c:\\',
+    dir: 'c:\\wamp\\www',
+    base: 'a.html',
+    ext: '.html',
+    name: 'a' }*/
